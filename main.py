a3253ad358522108068d40f3de8eba18d462b83c
Chernyshov_Nikita <xpom5566@mail.ru>
2020-04-14 19:19:48 +0300

    solved: 88ms (99.6%) / 17.2 (~30%) , uniq key generation with dict item

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+from typing import List
+
+
+class Solution:
+
+  def groupAnagrams(self, words: List[str]) -> List[List[str]]:
+    obj = {}
+    key = ''
+
+    for word in words:
+      # generate key for each word
+      key = '/'.join(sorted([char for char in word]))
+      # accumulate words in obj (dict)
+      if key not in obj:
+        obj[key] = [word]
+      else:
+        obj[key].append(word)
+
+    return obj.values()
+
+
 my = Solution()
-n = 0
-ans = my.(n)
+n = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ans = my.groupAnagrams(n)
 print("ans", ans)
